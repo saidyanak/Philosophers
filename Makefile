adc7a144b599ace6fe1823eb2236f25ca1c9b0be
saidyanak <said.ynk@gmail.com>
2025-08-25 14:50:32 +0300

    Böyle Buyurdu Zerdüşt

--- Makefile
+++ Makefile
@@ -1,41 +1,27 @@
 NAME = philo
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror -pthread  -g -fsanitize=thread -O0
 RM = rm -rf
 OBJ = $(SRC:.c=.o)
 SRC = main.c
 OBJ_DIR = obj
-LIBFTDIR = libft
-LIBFT = $(LIBFTDIR)/libft.a
-LIBFT_FLAGS = -L$(LIBFTDIR) -lft
 
 OBJ = $(addprefix $(OBJ_DIR)/, $(SRC:.c=.o))
-
-# Color codes
-GREEN = \033[0;32m
-RED = \033[0;31m
-YELLOW = \033[0;33m
-RESET = \033[0m
 
 all: $(NAME)
 
 $(NAME): $(LIBFT) $(OBJ)
-	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ) $(LIBFT_FLAGS)  && echo "$(GREEN)✅ Compilation successful!$(RESET)" || echo "$(RED)❌ Compilation failed!$(RESET)"
+	@$(CC) $(CFLAGS) -o $(NAME) $(OBJ)
 
 $(OBJ_DIR)/%.o: %.c
 	@mkdir -p $(OBJ_DIR)
 	@$(CC) $(CFLAGS) -c $< -o $@
 
-$(LIBFT):
-	@make -C $(LIBFTDIR)
-
 clean:
 	@rm -rf $(OBJDIR)
-	@make clean -C $(LIBFTDIR)
 
 fclean: clean
 	@rm -f $(NAME)
-	@make fclean -C $(LIBFTDIR)
 
 re: fclean all
 
--- phliosophers.h
+++ phliosophers.h
@@ -6,13 +6,17 @@
 /*   By: syanak <syanak@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 10:13:47 by syanak            #+#    #+#             */
-/*   Updated: 2025/08/25 10:31:43 by syanak           ###   ########.fr       */
+/*   Updated: 2025/08/25 13:56:39 by syanak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 # include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
 typedef struct s_data
 {
@@ -24,6 +28,7 @@ typedef struct s_data
 	int				stop_simulation;
 	int				all_ate_enough;
 	long long		start_time;
+	long long		end_time;
 	pthread_mutex_t	print_mutex;
 	pthread_mutex_t	death_mutex;
 	pthread_mutex_t	meal_mutex;
@@ -33,8 +38,8 @@ typedef struct s_data
 typedef struct s_philo
 {
 	int				id;
-	int				meals_eath;
-	long long		last_meat_eath;
+	int				meals_eaten;
+	long long		last_meal_time;
 	pthread_t		thread;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
@@ -42,5 +47,15 @@ typedef struct s_philo
 	struct s_philo	*prev;
 	t_data			*data;
 }					t_philo;
+
+// Fonksiyon prototipleri
+long long			get_time(void);
+void				ft_usleep(long long time);
+void				print_status(t_philo *philo, char *status);
+int					check_philosopher_death(t_philo *philo);
+int					check_all_ate_enough(t_philo *first);
+void				*philosopher_routine(void *arg);
+int					start_threads(t_philo *first);
+void				join_threads(t_philo *first);
 
 #endif
